5b3c823e7454de166573f34a7511fd17191abc64
nelcambo <nelcambo@gmail.com>
2022-08-25 19:31:26 -0500

    Initial commit

---
+++ src/main/java/com/Ciclo3MinTic/Proyecto01SpringBoot/Proyecto01SpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.Ciclo3MinTic.Proyecto01SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto01SpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Proyecto01SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ciclo3MinTic/Proyecto01SpringBoot/Proyecto01SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ciclo3MinTic.Proyecto01SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto01SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
